6f34b902242c69569d8a5f978392b68eb53db4f4
= <=>
2022-06-29 18:47:57 +0100

    update: wrote and implemented placeholder chakra navbar

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Navbar from '../navbar.js'
 import { Box, Container } from '@chakra-ui/react'
 
 
@@ -9,6 +10,8 @@ const Main = ({ children, router }) => {
         <meta name={"viewport"} content={"width=device-width, initial-scale=1"} />
         <title>Raf Shahid - Homepage</title>
       </Head>
+
+      <Navbar path={router.asPath} />
 
       <Container maxW={"container.md"} pt={14}>
         {children}
