73fc6d5ebcbbf24bc872afc65f6d6a4fb65f6fa4
rraafays <rraf@tuta.io>
2022-09-05 10:13:33 +0100

    update: integrated chakra ui

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
   return (
-    <div>Hello</div>
+    <Container>
+      <Box borderRadius={'lg'} bg={'#ebdbb2'} color={'#3c3836'} p={3} mb={6} align={'center'}>
+        Hey, I'm Raf, a full-stack developer based in Leeds
+      </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as={'h2'} variant={'page-title'}>
+            Raf Shahid
+          </Heading>
+          <p>Digital Chef ( Developer / Painter / Cook )</p>
+        </Box>
+      </Box>
+    </Container>
   )
 }
 
