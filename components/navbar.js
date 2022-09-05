95c0c6e517a5669b566d940cb70c38fcd78a2802
rraafays <rraf@tuta.io>
2022-09-05 15:11:55 +0100

    update: created theme toggle button, need to fix background of the button

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,7 @@
 import Logo from './logo'
 import NextLink from 'next/link'
 import { HamburgerIcon } from '@chakra-ui/icons'
+import ThemeToggleButton from './theme-toggle-button'
 import {
   Container,
   Box,
@@ -74,6 +75,7 @@ const Navbar = props => {
           </LinkItem>
         </Stack>
         <Box flex={1} align={'right'}>
+          <ThemeToggleButton />
           <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
             <Menu>
               <MenuButton
