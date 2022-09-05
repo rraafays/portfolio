b76d93aac6de0c11f79b4485bdae64bae5d400bd
rraafays <rraf@tuta.io>
2022-09-05 13:53:55 +0100

    update: added menu items to the hamburger menu

--- components/navbar.js
+++ components/navbar.js
@@ -82,7 +82,18 @@ const Navbar = props => {
                 variant={'outline'}
                 aria-label={'Options'}
               />
-
+              <MenuList>
+                <NextLink href={'/'} passHref>
+                  <MenuItem as={Link}>About</MenuItem>
+                </NextLink>
+                <NextLink href={'/works'} passHref>
+                  <MenuItem as={Link}>Works</MenuItem>
+                </NextLink>
+                <NextLink href={'/posts'} passHref>
+                  <MenuItem as={Link}>Posts</MenuItem>
+                </NextLink>
+                <MenuItem as={Link} href={'https://www.github.com/rraafays/portfolio'}>source code</MenuItem>
+              </MenuList>
             </Menu>
           </Box>
         </Box>
