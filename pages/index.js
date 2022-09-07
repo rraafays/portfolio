1e4c7c1332fa424320cf93eded831c82edf6f151
rraafays <rraf@tuta.io>
2022-09-07 03:38:58 +0100

    update: added an about me paragraph, need to get portfolio button working but tired. zzz

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
+import { Container, Box, Button, Heading, Link, Image, useColorModeValue } from '@chakra-ui/react'
+import NextLink from 'next/link'
 import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
 
 const Page = () => {
   const grayColorI = useColorModeValue('#7C6F64', '#A89984')
@@ -34,11 +37,23 @@ const Page = () => {
         <Heading
           as={'h3'}
           variant={'section-title'}
-          textDecorationColor={useColorModeValue('#FE8019', '#AF3A03')}
+          textDecorationColor={useColorModeValue('#AF3A03', '#FE8019')}
         >
           About
         </Heading>
-        <p>placeholder</p>
+        <Paragraph>
+          Raf is a software student and aspiring full-stack developer based in Leeds with a passion for building digital services/solutions to tackle life's challenges.
+          He has a knack for spotting a problem and creating a solution, from the planning and designing stage all the way to development.
+          Away from the keyboard, he loves creating artworks, exploring music & late night drives.
+          Currently, he is studying at {' '} <Link href='https://www.shu.ac.uk' color={useColorModeValue('#8F3F71', '#D3869B')}>Sheffield Hallam University</Link>.
+        </Paragraph>
+        <Box align={'center'} my={4}>
+          <NextLink href={'/works'}>
+            <Button rightIcon={<ChevronRightIcon />} bgColor={useColorModeValue('#076678', '#83A598')}>
+              My Portfolio
+            </Button>
+          </NextLink>
+        </Box>
       </Section>
     </Container>
   )
