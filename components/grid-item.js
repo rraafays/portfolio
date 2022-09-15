73b69dc77e32de71e7e017898d5efbbe41cb33a3
rraafays <rraf@tuta.io>
2022-09-15 22:12:45 +0100

    update: renamed grid-item objects correctly, updated dependencies. still trying to solve error when rendoring project grid itme

--- components/grid-item.js
+++ components/grid-item.js
@@ -4,7 +4,7 @@ import { Box, Text, LinkOverlay, LinkBox } from '@chakra-ui/react'
 import Global from '@emotion/react'
 
 export const GridItem = ({ children, href, title, thumbnail }) => (
-  <Box w={'100%'} align={'center'}>
+  <Box w={'100%'} textAlign={'center'}>
     <LinkBox cursor={'pointer'}>
       <Image
         src={thumbnail}
@@ -22,7 +22,7 @@ export const GridItem = ({ children, href, title, thumbnail }) => (
 )
 
 export const ProjectGridItem = ({ children, id, title, thumbnail }) => (
-  <Box w={'100%'} align={'center'}>
+  <Box w={'100%'} textAlign={'center'}>
     <NextLink href={`/projects/${id}`} passHref scroll={false}>
       <LinkBox cursor={'pointer'}>
         <Image
--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,6 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
-import WorkGridItem from '../components/grid-item'
+import ProjectGridItem from '../components/grid-item'
 import GentooThumb from '../public/images/projects/GentooThumb.png'
 
 const Projects = () => {
@@ -11,9 +11,9 @@ const Projects = () => {
       </Heading>
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-          <WorkGridItem id={'Gentoo'} title={'Gentoo'} thumbnail={GentooThumb}>
+          <ProjectGridItem id={'Gentoo'} title={'Gentoo'} thumbnail={GentooThumb}>
             My portage configuration for my Gentoo Linux Development system
-          </WorkGridItem>
+          </ProjectGridItem>
         </Section>
       </SimpleGrid>
     </Container>
