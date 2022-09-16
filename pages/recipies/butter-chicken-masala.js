d944d402a5af5f014d16eca407fd532a04f14af7
raf <rraf@tuta.io>
2022-09-16 17:19:07 +0100

    update: added instructions to butter chicken masala

--- pages/recipies/butter-chicken-masala.js
+++ pages/recipies/butter-chicken-masala.js
@@ -11,6 +11,7 @@ import {
 } from '@chakra-ui/react'
 import { MinusIcon } from '@chakra-ui/icons'
 import Section from '../../components/section'
+import Paragraph from '../../components/paragraph'
 
 const Page = () => {
   return (
@@ -53,6 +54,35 @@ const Page = () => {
           <ListItem><MinusIcon />&ensp;sugar - 1tbsp</ListItem>
           <ListItem><MinusIcon />&ensp;fenugreek seeds - 1/2tbsp</ListItem>
         </List>
+      </Section>
+      <Section delay={0.2}>
+        <Paragraph>
+          <b>1</b>&emsp;in a bowl, combine chicken with all of the ingredients for the chicken marinade; let marinate for 30 minutes to an hour (or overnight if time allows)
+        </Paragraph>
+        <Paragraph>
+          <b>2</b>&emsp;heat oil in a large skillet or pot over medium-high heat. When sizzling, add chicken pieces in batches of two or three, making sure not to crowd the pan. Fry until browned for only 3 minutes on each side. Set aside and keep warm. (you will finish cooking the chicken in the sauce.)
+        </Paragraph>
+        <Paragraph>
+          <b>3</b>&emsp;heat butter or ghee in the same pan. Fry the onions until they start to sweat (about 6 minutes) while scraping up any browned bits stuck on the bottom of the pan
+        </Paragraph>
+        <Paragraph>
+          <b>4</b>&emsp;add garlic and ginger and sauté for 1 minute until fragrant, then add ground coriander, cumin and garam masala. Let cook for about 20 seconds until fragrant, while stirring occasionally
+        </Paragraph>
+        <Paragraph>
+          <b>5</b>&emsp;add crushed tomatoes, chili powder and salt. Let simmer for about 10-15 minutes, stirring occasionally until sauce thickens and becomes a deep brown red colour
+        </Paragraph>
+        <Paragraph>
+          <b>6</b>&emsp;remove from heat, scoop mixture into a blender and blend until smooth. You may need to add a couple tablespoons of water to help it blend (up to 1/4 cup). Work in batches depending on the size of your blender
+        </Paragraph>
+        <Paragraph>
+          <b>7</b>&emsp;pour the puréed sauce back into the pan. Stir the cream, sugar and crushed kasoori methi (or fenugreek leaves) through the sauce. Add the chicken with juices back into the pan and cook for an additional 8-10 minutes until chicken is cooked through and the sauce is thick and bubbling
+        </Paragraph>
+        <Paragraph>
+          <b>8</b>&emsp;garnish with chopped cilantro and serve with fresh, naan
+        </Paragraph>
+        <Paragraph>
+          <b>9</b>&emsp;enjoy with a can of mirinda
+        </Paragraph>
       </Section>
     </Container>
   )
