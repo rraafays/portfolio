4d12d8a6e970103eee95b316a0c54159fdece162
rraafays <rraf@tuta.io>
2022-09-24 22:18:13 +0100

    update: wrote custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,26 @@
+import NextLink from 'next/link'
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Divider,
+  Button
+} from '@chakra-ui/react'
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Heading as={'h1'} align={'center'} fontSize={180}>404</Heading>
+      <Text align={'center'}>the world is not ready for this page.</Text>
+      <Divider my={6} />
+      <Box my={6} align={'center'}>
+        <NextLink href={"/"}>
+          <Button>Home</Button>
+        </NextLink>
+      </Box>
+    </Container>
+  )
+}
+
+export default NotFound
